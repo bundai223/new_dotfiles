a047de4894b061b6a0cf988a55bae1ee69be338d
Daiji Nishimura <daiji@DaijinoMacBook-Pro.local>
2016-12-03 20:45:39 +0900

    rm comment

--- dotfiles.sh
+++ dotfiles.sh
@@ -10,73 +10,80 @@ fi
 
 # brew install
 
-#/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-##
-#brew tap caskroom/cask
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 #
-#caskapp=(
-# google-chrome
-# android-file-transfer
-# 1password
-# dropbox
-# skype
-# steam
-# cooviewer
-# vlc
-# iterm2
-# dash
-# kobito
-# android-studio
-# virtualbox
-# vagrant
-# java
-# docker
-# utorrent
-#)
-#brew cask install ${caskapp[@]}
-#
-#cellers=(
-# wget
-# coreutils
-# findutils
-# luajit
-# lua
-# git
-# mercurial
-# zsh
-# z
-# tmux
-# the_silver_searcher
-# reattach-to-user-namespace
-# python
-# rbenv
-# go
-# neovim/neovim/neovim
-#)
-#
-#brew install ${cellers[@]}
-#
-#brew cleanup
-#
-#pip3 install --upgrade pip setuptools wheel
-#pip3 install --upgrade neovim
-#
-#rbenv install 2.3.1
-#rbenv global 2.3.1
+brew tap caskroom/cask
 
-#gem install rsense
+caskapp=(
+ google-chrome
+ android-file-transfer
+ 1password
+ dropbox
+ skype
+ steam
+ cooviewer
+ vlc
+ iterm2
+ dash
+ kobito
+ android-studio
+ virtualbox
+ vagrant
+ java
+ docker
+ utorrent
+)
+brew cask install ${caskapp[@]}
 
-#curl https://sh.rustup.rs -sSf | sh
-#source $HOME/.cargo/env
-#cargo install racer
-#cargo install rustfmt
-#rustup component add rust-src
+cellers=(
+ wget
+ coreutils
+ findutils
+ luajit
+ lua
+ git
+ mercurial
+ zsh
+ z
+ tmux
+ the_silver_searcher
+ reattach-to-user-namespace
+ python
+ rbenv
+ go
+ neovim/neovim/neovim
+)
 
-NVIM_CONF=$CONF_BASE/nvim
-mkdir -p $NVIM_CONF/dein
-#curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer_dein.sh
-#sh installer_dein.sh $NVIM_CONF/dein
-#rm installer_dein.sh
+brew install ${cellers[@]}
 
+brew cleanup
+
+pip3 install --upgrade pip setuptools wheel
+pip3 install --upgrade neovim
+pip install --upgrade pip setuptools wheel
+pip install --upgrade neovim
+
+rbenv install 2.3.1
+rbenv global 2.3.1
+
+# ruby
+gem install rsense
+gem install neovim
+
+# rust
+curl https://sh.rustup.rs -sSf | sh
+source $HOME/.cargo/env
+cargo install racer
+cargo install rustfmt
+rustup component add rust-src
+
+# repos
 mkdir -p ~/repos/github.com/bundai223/
 git clone git@github.com:bundai223/dotfiles.git ~/repos/github.com/bundai223/dotfiles
+git clone git@github.com:bundai223/new_dotfiles.git ~/repos/github.com/bundai223/
+git clone git@github.com:bundai223/zshrc.git ~/repos/github.com/bundai223/
+git clone git@github.com:bundai223/init.vim.git ~/repos/github.com/bundai223/
+git clone git@github.com:bundai223/tmux.conf.git ~/repos/github.com/bundai223/
+
+# nvim
+

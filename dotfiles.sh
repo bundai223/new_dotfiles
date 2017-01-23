4549864ce89fa4d7dcab82cc80f7d9fc3fbe238a
Example Default <default@example.jp>
2017-01-23 09:49:56 +0900

    update install scripts

--- dotfiles.sh
+++ dotfiles.sh
@@ -8,83 +8,166 @@ if [ -n $CONF_BASE ]; then
   mkdir -p $CONF_BASE
 fi
 
-# brew install
+if [ "$(uname)" == 'Darwin' ]; then
+  OS='Mac'
+  brew_install
+elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
+  OS='Linux'
+  go_install
+elif [ "$(expr substr $(uname -s) 1 10)" == 'MINGW32_NT' ]; then
+  OS='Cygwin'
+else
+  echo "Your platform ($(uname -a)) is not supported."
+  exit 1
+fi
 
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-#
-brew tap caskroom/cask
+pip_install
+ruby_install
+gem_install
 
-caskapp=(
- google-chrome
- android-file-transfer
- 1password
- dropbox
- skype
- steam
- cooviewer
- vlc
- iterm2
- dash
- kobito
- android-studio
- virtualbox
- vagrant
- java
- docker
- utorrent
-)
-brew cask install ${caskapp[@]}
 
-cellers=(
- wget
- coreutils
- findutils
- luajit
- lua
- git
- mercurial
- zsh
- z
- tmux
- the_silver_searcher
- reattach-to-user-namespace
- python
- rbenv
- go
- neovim/neovim/neovim
-)
 
-brew install ${cellers[@]}
+brew_install()
+{
+  # brew install
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  #
+  brew tap caskroom/cask
+  
+  caskapp=(
+   google-chrome
+   android-file-transfer
+   1password
+   dropbox
+   skype
+   steam
+   cooviewer
+   vlc
+   iterm2
+   dash
+   kobito
+   android-studio
+   virtualbox
+   vagrant
+   java
+   docker
+   utorrent
+  )
+  brew cask install ${caskapp[@]}
+  
+  cellers=(
+   wget
+   coreutils
+   findutils
+   luajit
+   lua
+   git
+   mercurial
+   zsh
+   z
+   tmux
+   the_silver_searcher
+   reattach-to-user-namespace
+   python
+   rbenv
+   go
+   neovim/neovim/neovim
+  )
+  
+  brew install ${cellers[@]}
+  
+  brew cleanup
+}
 
-brew cleanup
-
-pip3 install --upgrade pip setuptools wheel
-pip3 install --upgrade neovim
-pip install --upgrade pip setuptools wheel
-pip install --upgrade neovim
-
-rbenv install 2.3.1
-rbenv global 2.3.1
+# pip_install
+pip_install()
+{
+  pip3 install --upgrade pip setuptools wheel
+  pip3 install --upgrade neovim
+  pip install --upgrade pip setuptools wheel
+  pip install --upgrade neovim
+}
 
 # ruby
-gem install rsense
-gem install neovim
+ruby_install()
+{
+  rbenv install 2.3.1
+  rbenv global 2.3.1
+}
+
+gem_install()
+{
+  gem install rsense
+  gem install neovim
+}
 
 # rust
-curl https://sh.rustup.rs -sSf | sh
-source $HOME/.cargo/env
-cargo install racer
-cargo install rustfmt
-rustup component add rust-src
+rust_install()
+{
+  curl https://sh.rustup.rs -sSf | sh
+  source $HOME/.cargo/env
+  cargo install racer
+  cargo install rustfmt
+  rustup component add rust-src
+}
+
+go_install()
+{
+  # install go
+  #OS_TYPE=linux-386
+  OS_TYPE=linux-amd64
+  GO_VERSION=1.7.4
+  ZIPNAME=go${GO_VERSION}.${OS_TYPE}.tar.gz
+  set +eu
+  CHECK_VERSION=$(go version|grep ${GO_VERSION})
+  set -eu
+  if [ -z "${CHECK_VERSION}" ]; then
+    wget http://golang.org/dl/${ZIPNAME}
+    sudo tar zxvf ${ZIPNAME} -C /usr/local
+    rm ${ZIPNAME}
+  fi
+
+  PATH=/usr/local/go/bin/:$PATH
+  export PATH
+}
 
 # repos
-mkdir -p ~/repos/github.com/bundai223/
-cd ~/repos/github.com/bundai223/
-git clone git@github.com:bundai223/dotfiles.git
-git clone git@github.com:bundai223/new_dotfiles.git
-git clone git@github.com:bundai223/zshrc.git
-git clone git@github.com:bundai223/init.vim.git
-git clone git@github.com:bundai223/tmux.conf.git
+repos=(\
+ dotfiles\
+ new_dotfiles\
+ zshrc\
+ init.vim\
+ tmux.conf\
+)
 
+own_repos_dir=~/repos/github.com/bundai223/
+mkdir -p $own_repos_dir
+cd $own_repos_dir
+
+github_base_url=https://github.com
+
+for repos in ${OWN_REPOS[@]}; do
+  git clone ${github_base_url}/bundai223/${repos}
+done
 # nvim
+
+# Create symbolic link to dotfiles
+DOTFILES_ENTITY_PATH=~/repos/github.com/bundai223/new_dotfiles
+DOTFILE_NAMES_ARRAY=\
+(\
+ .gitconfig_global\
+ .gitignore_global\
+ .gitattributes_global\
+ .config/git/templates\
+ .ctags\
+)
+
+for dotfile in ${DOTFILE_NAMES_ARRAY[@]}; do
+  ln -s ${DOTFILES_ENTITY_PATH}/${dotfile} ~/${dotfile}
+done
+
+# Make git configuration file that include common config to make local setting.
+if [ ! -e ~/.gitconfig ]; then
+    cp ${DOTFILES_ENTITY_PATH}/.gitconfig_local ~/.gitconfig
+fi
 
